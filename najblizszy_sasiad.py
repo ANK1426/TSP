1ec27f3804698b97d109f5ddfc8d5f6893c63402
acbk01 <acbk06@gmail.com>
2025-05-29 11:20:56 +0200

    .

--- najblizszy_sasiad.py
+++ najblizszy_sasiad.py
@@ -1,4 +1,27 @@
 from wykresy import wykres
+start=[]
+dist=[]
+way=[]
+
+
+def wykonanie(punkty):
+    for i in range(len(punkty)):
+        nowe_punkty = punkty[:]  # kopia listy
+        nowe_punkty[0], nowe_punkty[i] = nowe_punkty[i], nowe_punkty[0]  # zamień pierwszy z i-tym
+        print(f"Startowy punkt: {nowe_punkty[0]}")
+        dlugosc, trasa = najblizszy_sasiad(nowe_punkty)
+        print(f"Długość trasy: {dlugosc}")
+        print(f"Trasa: {trasa}\n")
+        start.append(nowe_punkty[0])
+        dist.append(dlugosc)
+        way.append(trasa)
+
+    wykres(start,way,dist)
+    print(way)
+    print(dist)
+    print(start)
+
+
 
 
 def najblizszy_sasiad(punkty):
@@ -19,7 +42,7 @@ def najblizszy_sasiad(punkty):
     odleglosc=odleglosc+distance(trasa[-1],trasa[0])
     
     trasa.append(trasa[0])
-    wykres(trasa)
+    
     return  odleglosc , trasa
 
 
@@ -28,4 +51,4 @@ def distance(a,b):
     return ((a[0]-b[0])**2+(a[1]-b[1])**2)**(0.5)
 
 
-print(najblizszy_sasiad([[1,2],[4,5],[0,4],[5,5]]))
+print(wykonanie([[1,2],[4,5],[0,4],[5,5]]))
--- wykresy.py
+++ wykresy.py
@@ -1,12 +1,27 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-def wykres(a):
-    x= [punkt[0] for punkt in a]
-    y= [punkt[1] for punkt in a]
-    plt.plot(x,y,color='blue',label='Punkty',linestyle='-',marker='o')
+
+def wykres(start, way, dist):
+    fig, axs = plt.subplots(2, 2, figsize=(10, 8))
+
+    for i, ax in enumerate(axs.flat):
+        # Wyciągamy współrzędne x i y z way[i]
+        x_vals = [pt[0] for pt in way[i]]
+        y_vals = [pt[1] for pt in way[i]]
+
+        # Rysujemy trasę
+        ax.plot(x_vals, y_vals, marker='o', linestyle='-')
+
+        # Rysujemy punkt startowy na czerwono
+        ax.plot(start[i][0], start[i][1], 'ro', label='Start')
+
+        # Ustawiamy tytuł z odległością
+        ax.set_title(f'Długość: {dist[i]}')
+
+        # (opcjonalnie) dodaj siatkę i legendę
+        ax.grid(True)
+        ax.legend()
+
+    plt.tight_layout()
     plt.show()
-
-
-
-   
