2b5e51f03e38d318748a41d34f9baf7bc65aa743
ANK1426 <a.kampa@student.uw.edu.pl>
2025-06-05 15:28:51 +0200

    Update wykresy.py

--- wykresy.py
+++ wykresy.py
@@ -1,24 +1,21 @@
-import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt #pobieramy biblioteki
 import numpy as np
 
-import matplotlib.pyplot as plt
-
 def wykres(way, dist):
-    etykiety = ["Brute Force", "Nearest Neighbor", "NN + 2-opt"]
+    tytuly = ["Brute Force", "Nearest Neighbor", "NN + 2-opt"]
     
-    fig, axes = plt.subplots(1, 3, figsize=(18, 6))
+    fig, axes = plt.subplots(1, 3, figsize=(18, 6)) #layout wykresu
     
-    for i in range(3):
+    for i in range(3): 
         trasa = way[i]
         
-        
-        x = [point[0] for point in trasa] #
+        x = [point[0] for point in trasa] #dzielimy punkty z trasy na współrzędne x,y
         y = [point[1] for point in trasa]
         
-        
+        #po czym je wypisujemy
         axes[i].plot(x, y, marker='o', linestyle='-', color='blue', markersize=8)
         
-        axes[i].set_title(f"{etykiety[i]}\nDistance: {dist[i]:.2f}") #tutaj dodajemy ozdobniki wizualne
+        axes[i].set_title(f"{tytuly[i]}\nDistance: {dist[i]:.2f}") #tutaj dodajemy ozdobniki wizualne
         axes[i].set_xlabel("x")
         axes[i].set_ylabel("y")
         axes[i].grid(True)
